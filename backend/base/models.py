65d5fe950049b69a235cc186420c57806e7aea70
Swapnila Kavishwar <62277537+swap-stack@users.noreply.github.com>
2022-04-05 18:47:23 +0530

    Renamed field countInStock and Products url
    Renamed countInStock field and migrated the changes to db. Modified Products url endpoint from /api/product/id to /api/products/id

--- backend/base/models.py
+++ backend/base/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(max_digits=7, decimal_places=2,null=True, blank=True)
-    countinStock = models.IntegerField(null=True, blank=True, default=0)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
