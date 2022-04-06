0fa43d52834deaa65aa34b80140e818be9c8aa98
Swapnila Kavishwar <62277537+swap-stack@users.noreply.github.com>
2022-04-06 16:00:53 +0530

    Added serializers, urls and views for Product and User
    Added login, register and view profile functionality. Moved Product views and urls to a different file. Added JWT tokens

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,7 +1,46 @@
+from asyncore import read
+from lib2to3.pgen2 import token
+from pickletools import read_floatnl
 from rest_framework import serializers
-from .models import Product
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import Product, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "_id", "username", "email", "name", "isAdmin"]
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    def get__id(self, obj):
+        return obj.id
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == "":
+            name = obj.email
+        return name
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "_id", "username", "email", "name", "isAdmin", "token"]
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = "__all__"
--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,24 +1,15 @@
-from math import prod
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from base.models import Product
+from base.serializers import ProductSerializer
 
-from .models import Product
-from .serializers import ProductSerializer
-# from .products import products
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-
-# Create your views here.
-
-
-@api_view(["GET"])
-def getRoutes(request):
-    routes = [
-        'api/routes',
-        'api/products',
-        'api/product',
-
-    ]
-    return Response(routes)
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 
 @api_view(["GET"])
